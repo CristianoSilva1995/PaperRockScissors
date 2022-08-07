dd30327fcd6a2527b2eabedde988bf533dafbff7
Cristiano Silva <ukratpt95@gmail.com>
2022-08-07 23:06:19 +0100

    Design minor corrections Game implementation   - AI, implemented sequences

--- Class/CPU.java
+++ Class/CPU.java
@@ -1,9 +1,19 @@
 package Class;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class CPU {
     Random random = new Random();
+
+    static ArrayList<String> playerChoicesTracker = new ArrayList<>();
+    String[] sequence_1 = {"rock","paper","scissors"};
+    String[] sequence_2 = {"paper","scissors","rock"};
+    String[] sequence_3 = {"scissors","rock","paper"};
+    String[] sequence_4 = {"rock","paper","rock"};
+    String[] sequence_5 = {"paper","scissors","paper"};
+    String[] sequence_6 = {"scissors","rock","scissors"};
+    String[] sequence_7 = {"scissors","paper","rock"};
     private String choice;
     private int winsCounter = 0;
 
@@ -22,8 +32,53 @@ public class CPU {
     public void setWinsCounter() {
         this.winsCounter++;
     }
+    public void setPlayerChoicesTracker(String playerChoice) {
+        this.playerChoicesTracker.add(playerChoice);
+    }
 
     public String generateCPUChoice(String[] AVAILABLE_CHOICES){
+        System.out.println(playerChoicesTracker);
+        if(playerChoicesTracker.size() < 3){
+            return AVAILABLE_CHOICES[random.nextInt(AVAILABLE_CHOICES.length)];
+        }else if(playerChoicesTracker.get(playerChoicesTracker.size() - 3).equals(playerChoicesTracker.get(playerChoicesTracker.size() - 2))){
+            if(playerChoicesTracker.get(playerChoicesTracker.size() - 2).equals(playerChoicesTracker.get(playerChoicesTracker.size() - 1))) {
+                if (playerChoicesTracker.get(playerChoicesTracker.size() - 1).equals("rock")) {
+                    return "paper";
+                } else if (playerChoicesTracker.get(playerChoicesTracker.size() - 1).equals("paper")) {
+                    return "scissors";
+                } else {
+                    return "rock";
+                }
+            }
+        }else if(verifySequence(sequence_1)){
+            return "paper";
+        }else if(verifySequence(sequence_2)){
+            return "scissors";
+        }else if(verifySequence(sequence_3)){
+            return "rock";
+        }else if(verifySequence(sequence_4)){
+            return "scissors";
+        }else if(verifySequence(sequence_5)){
+            return "rock";
+        }else if(verifySequence(sequence_6)){
+            return "paper";
+        }else if(verifySequence(sequence_7)){
+            System.out.println("sequence!");
+            return "rock";
+        }
         return AVAILABLE_CHOICES[random.nextInt(AVAILABLE_CHOICES.length)];
+    }
+
+    private Boolean verifySequence(String[] sequence){
+        int numberOfChoicesToTrack = 2;
+        Boolean response = false;
+        for(int i = 0; i< numberOfChoicesToTrack; i++){
+            if(sequence[i] == playerChoicesTracker.get(playerChoicesTracker.size() - (i + 1))){
+                response = true;
+            }else {
+                response = false;
+            }
+        }
+        return response;
     }
 }
